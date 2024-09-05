89c10031b20e9496823e8a31ea318c49df298ed1
mukund-gpt <abhishek.jupyter@gmail.com>
2024-09-05 09:43:40 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8000;
+
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`Server is Listening to port: ${port}`);
